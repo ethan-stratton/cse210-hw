8ee88187ec8a0eb43d5b22c722557c8143d09280
Ethan Stratton <ethanstratton@byui.edu>
2024-01-13 13:41:12 -0700

    made plus and minus, added functionality to A, F

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,28 +6,54 @@ class Program
     {
         Console.Write("What is your grade? ");
         string userGrade = Console.ReadLine();
-
         int grade = int.Parse(userGrade);
+
+        int lastDigit = grade % 10;
+        var plusOrMinus = (dynamic)null;
+
+        if (lastDigit >= 7 && grade <= 90 && grade >= 60)
+        {
+            plusOrMinus = '+';
+        }
+        else if (lastDigit < 3 && grade >= 60)
+        {
+            plusOrMinus = '-';
+        }
+        else
+        {
+            //plusOrMinus = (string)null;
+        }
+
+        //int[] array = { 1, 3, 5 };
+        //var lastItem = array[^1]; // 5
+
+        char letter = 'Z';
 
         if (grade >= 90 )
         {
-            Console.WriteLine("You got an 'A");
+            letter = 'A';
         }
         else if (grade >= 80)
         {
-            Console.WriteLine("You got a 'B");
+            letter = 'B';
         }
         else if (grade >= 70)
         {
-            Console.Write("You got a 'C'");
+            letter = 'C';
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("You got a 'D");
+            letter = 'D';
         }
         else 
         {
-            Console.WriteLine("You got an F");
+            letter = 'F';
+        }
+
+        Console.WriteLine($"You scored {letter}{plusOrMinus} in the class.");
+
+        if (grade >= 70){
+            Console.Write("Congratulations! You have passed the class!");
         }
 
     }

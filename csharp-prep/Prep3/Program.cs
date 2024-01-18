0049638404e208c7e7bb24d2b10ce6dc04ccb811
Ethan Stratton <ethanstratton@byui.edu>
2024-01-18 10:53:16 -0700

    made a number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,59 @@ using System;
 
 class Program
 {
+
+    static class count_stuff
+    {
+        public static int counter;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+        // In the Guess My Number game the computer picks a magic number, 
+        // and then the user tries to guess it. After each guess, 
+        // the computer tells the user to guess "higher" or "lower" until they guess the magic number.
+
+        //Console.Write("What is your magic number? ");
+        //string magNumber = Console.ReadLine();
+        //int target = int.Parse(magNumber);
+        string play_again_query;
+        do
+        {
+                    Random rnd = new Random();
+        int target = rnd.Next(0, 100);
+
+        Console.Write("What is your guess? ");
+        string user_guess = Console.ReadLine();
+        int guess = int.Parse(user_guess);        
+
+        count_stuff.counter = 0;
+
+        while (guess != target)
+        {
+            if (guess > target)
+            {
+                Console.Write("Your guess was too high. ");
+                Console.Write("What is your guess? ");
+                user_guess = Console.ReadLine();
+                guess = int.Parse(user_guess);
+                count_stuff.counter++;
+            }
+            else if (guess < target) 
+            {
+                Console.Write("Your guess was too low. ");
+                Console.Write("What is your guess? ");
+                user_guess = Console.ReadLine();
+                guess = int.Parse(user_guess);  
+                count_stuff.counter++; 
+            }
+        }
+        Console.WriteLine("Congratulations! You guessed the right number!");
+        Console.WriteLine($"Total guesses: {count_stuff.counter}");
+
+
+        Console.Write("Do you want to continue? Type 'yes' ");
+        play_again_query = Console.ReadLine();
+        } while (play_again_query == "yes");
+
+}
 }

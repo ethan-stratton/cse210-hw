cfdbb2fb89d42e15069b4654ac975b8407acaf3c
Ethan Stratton <ethanstratton@byui.edu>
2024-02-06 13:39:28 -0700

    fractions are working

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -15,13 +15,14 @@ class Program
         //using setters to change the values and then getters to retrieve these new values and then display 
         //them to the console.
 
+        //fraction 3 is 6 / 7
         Console.WriteLine(fraction3.GetTop());
         Console.WriteLine(fraction3.GetBottom());
 
         Console.WriteLine(fraction3.getFractionString());
         Console.WriteLine(fraction3.getDecimalValue());
 
-
+        //fraction 3 gets changed to 10 / 5
         fraction3.SetTop(10);
         fraction3.SetBottom(5);
 
